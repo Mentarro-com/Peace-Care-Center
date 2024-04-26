87a0d55a825a3ee442316112a868d256a89fbfbc
Logith <logithramesh4444@gmail.com>
2024-04-26 15:04:23 +0530

    First Commit - LOGITH

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Service from '../components/Service'
 
 const ServicePage = () => {
   return (
-    <div className='text-black mt-16'>ServicePage</div>
+    <div className='text-black mt-16'><Service /></div>
   )
 }
 
